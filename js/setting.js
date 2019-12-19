bd6eba4a2f11446633fad2aafa274b9326c4d84f
Natsuko YOSHIGA <2604408+nikolito@users.noreply.github.com>
2019-12-19 11:41:21 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -9,4 +9,4 @@ var MaxDescription = 9; // ごみの最大種類、９を超えない場合は�
 
 var MaxMonth = 8; // 何ヶ月先まで計算するかを指定します。
 
-var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+var WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
